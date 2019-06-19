b54c216a0763c3ed0f02e5a709a0ed44cb973167
Becher <mbelkhenchir@gmail.com>
2019-06-19 22:41:35 +0200

    2 puzzle

--- Assets/Scripts/Character/State/Jump.cs
+++ Assets/Scripts/Character/State/Jump.cs
@@ -9,7 +9,7 @@ public class Jump : State
         
         character.gameObject.layer = 9;
         character.rigidbody.velocity = new Vector3(0, 0);
-        character.rigidbody.AddForce(character.transform.up * 300);
+        character.rigidbody.AddForce(character.transform.up * 450);
     }
     public override string GetIdentifiant()
     {
